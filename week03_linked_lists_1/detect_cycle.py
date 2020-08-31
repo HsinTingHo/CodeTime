c364130de39aefbc16de234a090323e4f4dc869f
repl.it user <replituser@example.com>
2020-08-31 13:56:40 +0000

    implement singlely linked list with cycle

---
+++ week03_linked_lists_1/detect_cycle.py
@@ -0,0 +1,3 @@
+# Problem 2 - Detect Cycle
+# Given a linked list, determine if it has a cycle in it. For this problem, it may be helpful for you to write some pseudocode on how to build a linked list with a cycle. A linked list with a cycle may look something like:
+
--- week03_linked_lists_1/linked_list.py
+++ week03_linked_lists_1/linked_list.py
@@ -27,3 +27,38 @@ class S_List:
 #  total time: 3m22s
 #********************************
 
+
+#A single linked list with tail connected to head to form a cycle
+class S_Cycle_List:
+  def __init__(self):
+    self.head = None
+    
+  def insert(self, node):
+    if self.head is None:
+      self.head = node
+      self.head.next = self.head
+    else:
+      current = self.head
+      while current.next is not self.head:
+        current = current.next
+      current.next = node
+      node.next = self.head
+
+  def print_list(self):
+    current = self.head
+    while current.next is not self.head:
+      print(current.data)
+      current = current.next
+    print(current.data)
+    print('tail connected to ',current.next.data)
+    
+print('***** Test Cycle List *****')
+test = [1,2,3,4,5]
+cycle_list = S_Cycle_List()
+for elem in test:
+  node = S_Node(elem)
+  cycle_list.insert(node)
+cycle_list.print_list()
+#********************************
+#  total time: 6m
+#********************************
