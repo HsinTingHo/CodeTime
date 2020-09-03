30e30d7f9d57475525125265f66487d7ed17d008
repl.it user <replituser@example.com>
2020-09-03 13:54:27 +0000

    add two numbers: Runtime = O(n) | Space = O(m)

--- week04_linked_lists_2/add_two_numbers.py
+++ week04_linked_lists_2/add_two_numbers.py
@@ -10,125 +10,104 @@
 #
 # Explanation: 342 + 465 = 807
 
-class Node:
-  def __init__(self, data=None):
-    self.data = data
-    self.next = None
-class Single_List:
-  def __init__(self):
-    self.head = None
+#Understand:
+# Can I alter one of the list? -> no
+# are we passing in list or head of the list? -> head
+# return head or list? -> list
 
-
-#test cases:
-new_list1 = Single_List()
-new_list2 = Single_List()
-new_lists = [new_list1, new_list2]
-
-test_input1 = [9,9]
-test_input2 = [9,9]
-inputs=[test_input1, test_input2]
-
-
-for index, each_input in enumerate(inputs):
-  count = 0
-  for num in each_input:
-    node = Node(num)
-    if count == 0:
-      new_lists[index].head = node
-      current = new_lists[index].head
-    else:
-      current.next = node
-      current = current.next
-    count += 1
-
-#print out list content to varify the list constructed correctly
-# for each_list in new_lists:
-#   current = each_list.head
-#   while current is not None:
-#     print(current.data)
-#     current = current.next
-# Problem 1 - Add Two Numbers
-# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
-# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-# Example:
-
+# edge case:
 # Input: 2 -> 4 -> 3,  5 -> 6 -> 4
 # Output: 7 -> 0 -> 8
 
-# Explanation: 342 + 465 = 807
+# Input: 2 -> 4,  5 -> 6 -> 4
+# Output: 7 -> 0 -> 5
 
+# Input: 1 -> 2, 0
+# Output: 1 -> 2
 
-#Understand
-# does both linked list has same length?
-# inplace? - No
+#Match:
 
-# Input 0->0->0, 0->0->0
-# Output 0->0->0
+#Plan:
+#create a new siglely linked list
+#p1 point at list1
+#p2 point at list2
+#iterate both list together by move both pointer forward until one pointer is None
+# add the data of two nodes that are pointed at and carry
+# if the sum is greater than 10, set carry to 1
+# else set carry to 0
+# store result in a new node and insert to result list
+#if p1 is not None, connect result to p1
+#if p2 is not none, connect result to p2
+import linked_list as l
 
-# Input 9->9, 9->9
-# Output 8->9->1
+def add_two_numbers(head1, head2):
+  result = l.S_List()
+  p1 = head1
+  p2 = head2
+  carry = 0
 
-#Match
-#   run through multiple times
-# v Dummy nodes
-
-#Plan
-#loop through the lists and add each node up
-#if dummy.data is one
-#sum = test_data1+test_data2+1
-#set dummy node to 0
-#else
-#sum = test_data1+test_data2
-#if the sum is greater than 9, set dummy.data to 1
-#create a new node with sum and add to linked list
-#After loop, if the dummy node is 1, add dummy node to the end of list
-#return list
-
-#Implement
-def Add_Two_Numbers(input1, input2):
-  current1 = input1.head
-  current2 = input2.head
-  new_list = Single_List()
-  current = new_list.head
-  dummy = Node(0)
-  count = 0
-  while (current1 is not None) and (current2 is not None):
-    num = current1.data + current2.data
-    if dummy.data == 1:
-      num += 1
-      dummy.data = 0
-
-    if num > 9:
-      dummy.data = 1
-      new_node = Node(num-10)
+  while (p1 is not None) and (p2 is not None):
+    sum = p1.data + p2.data + carry
+    if sum >= 10:
+      sum -= 10
+      carry = 1
     else:
-      new_node = Node(num)
+      carry = 0
 
-    if count == 0:
-      new_list.head = new_node
-      current = new_list.head
-    else:
-      current.next = new_node
-      current = current.next
-    current1 = current1.next
-    current2 = current2.next
-    count += 1
+    node = l.S_Node(sum)
+    result.insert(node)
+    p1 = p1.next
+    p2 = p2.next
+  
+  current = result.head
+  while current.next is not None:
+    current = current.next
+  if p1 is not None:
+    if carry == 1:
+      p1.data += 1
+    current.next = p1
+  elif p2 is not None:
+    if carry == 1:
+      p2.data += 1
+    current.next = p2
+  
+  return result
 
-  if dummy.data == 1:
-    current.next = dummy
-  return new_list
+# edge case:
+# Input: 2 -> 4 -> 3,  5 -> 6 -> 4
+# Output: 7 -> 0 -> 8
 
+# Input: 2 -> 4,  5 -> 6 -> 4
+# Output: 7 -> 0 -> 5
 
-result = Add_Two_Numbers(new_list1,new_list2)
-current = result.head
-while current is not None:
-  print(current.data)
-  current = current.next
+# Input: 1 -> 2, 0
+# Output: 1 -> 2
+#review
+input1 = [1,2]
+input2 = [0]
+test1 = l.S_List()
+test2 = l.S_List()
 
-#Review
+for elem in input1:
+  node = l.S_Node(elem)
+  test1.insert(node)
+
+for elem in input2:
+  node = l.S_Node(elem)
+  test2.insert(node)
+
+print('test1')
+test1.print_list()
+print('test2')
+test2.print_list()
+print('result')
+result = add_two_numbers(test1.head, test2.head)
+result.print_list()
 
 #Evaluate
-#runtime of the solution: O(n)
-#space complexity: O(n)
+#Runtime: O(n)  n is the size of the smaller list
+#Space: O(m) m is the size of the bigger list
+
+#***********************
+#  total time: 31m 54s
+#***********************
